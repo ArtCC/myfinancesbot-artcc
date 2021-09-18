6ccb2e1b01ebb0c93e376afc08b342297b999e55
Arturo Carretero Calvo <10163049+ArtCC@users.noreply.github.com>
2021-09-18 18:08:59 +0200

    Refactor

--- index.js
+++ index.js
@@ -1,38 +1,49 @@
 require("dotenv").config();
 
-const axios = require('axios');
 const TelegramBot = require("node-telegram-bot-api");
+
+const axios = require('axios');
 const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, {
      polling: true
 });
-const { Pool } = require('pg');
-const pool = new Pool({
-     connectionString: process.env.DATABASE_URL,
-     ssl: {
-          rejectUnauthorized: false
-     }
-});
-const formatter = new Intl.NumberFormat('de-DE', {
-     minimumFractionDigits: 2,
-     maximumFractionDigits: 8,
-});
-const startText = "\nEscribe la barra / para ver en qué te puedo ayudar.\n\nMás información: https://github.com/ArtCC/myfinancesbot-artcc";
-const revenueUpdateText = "Tus ingresos totales han sido actualizados correctamente.";
-const revenueInsertText = "Tus ingresos totales han sido añadidos correctamente.";
-const deleteUserText = "Tu usuario ha sido eliminado correctamente.";
-const generalErrorText = "¡Vaya! Parece que ha habido un problema con tu solicitud. Inténtalo de nuevo por favor.";
-const actionsTitleText = "Elige una de las opciones disponibles";
-const totalRevenueOptionText = "Ingresos";
-const zeroRevenueText = "Tus ingresos totales son 0 €.";
-const cancelText = "Cancelar";
-const cancelActionsText = "De acuerdo.";
+const constants = require('./src/constants');
+const cron = require('node-cron');
+const database = require('./src/database');
+const helpers = require('./src/helpers');
 
-bot.onText(/^\/start/, (msg) => {
+/**
+ * Telegram bot functions.
+ */
+bot.onText(/^\/acciones/, (msg) => {
      let chatId = msg.chat.id;
-     let name = msg.from.first_name;
-     let message = `¡Hola ${name}!${startText}`;
+
+     var buttonData = []               
+     buttonData.push({text: constants.totalRevenueOptionText, callback_data: constants.totalRevenueOptionText}); 
+     buttonData.push({text: constants.cancelText, callback_data: constants.cancelText});
+
+     let buttons = {
+          reply_markup: {
+               inline_keyboard: [
+                    buttonData
+               ]
+          }
+     }
      
-     bot.sendMessage(chatId, message);
+     bot.sendMessage(chatId, constants.actionsTitleText, buttons);
+});
+
+bot.onText(/^\/borrar/, (msg) => {
+     let chatId = msg.chat.id;
+     let userId = msg.from.id;
+     let deleteQuery = `delete from users where id = ${userId};`;
+
+     database.queryDatabase(deleteQuery).then(function (result) {
+          log(result);
+          bot.sendMessage(chatId, constants.deleteUserText);
+     }).catch(function (err) {
+          log(err);
+          sendErrorMessageToBot(chatId);
+     });
 });
 
 bot.onText(/^\/ingresos (.+)/, (msg, match) => {
@@ -44,45 +55,27 @@ bot.onText(/^\/ingresos (.+)/, (msg, match) => {
      let insertQuery = `insert into users (id,name,revenue,created_at) values (${userId},'${name}',${revenue},${createdAt});`;
      let updateQuery = `update users set revenue = ${revenue} where id = ${userId};`
 
-     queryDatabase(insertQuery).then(function (result) {
-          bot.sendMessage(chatId, revenueInsertText);
+     database.queryDatabase(insertQuery).then(function (result) {
+          log(result);
+          bot.sendMessage(chatId, constants.revenueInsertText);
      }).catch(function (err) {
-          queryDatabase(updateQuery).then(function (result) {
-               bot.sendMessage(chatId, revenueUpdateText);
+          log(err);
+          database.queryDatabase(updateQuery).then(function (result) {
+               log(result);
+               bot.sendMessage(chatId, constants.revenueUpdateText);
           }).catch(function (err) {
+               log(err);
                sendErrorMessageToBot(chatId);
           });
      });
 });
 
-bot.onText(/^\/acciones/, (msg) => {
+bot.onText(/^\/start/, (msg) => {
      let chatId = msg.chat.id;
-
-     var buttonData = []               
-     buttonData.push({text: totalRevenueOptionText, callback_data: totalRevenueOptionText}); 
-     buttonData.push({text: cancelText, callback_data: cancelText});
-
-     let buttons = {
-          reply_markup: {
-               inline_keyboard: [
-                    buttonData
-               ]
-          }
-     }
+     let name = msg.from.first_name;
+     let message = `¡Hola ${name}!${constants.startText}`;
      
-     bot.sendMessage(chatId, actionsTitleText, buttons);
-});
-
-bot.onText(/^\/borrar/, (msg) => {
-     let chatId = msg.chat.id;
-     let userId = msg.from.id;
-     let deleteQuery = `delete from users where id = ${userId};`;
-
-     queryDatabase(deleteQuery).then(function (result) {
-          bot.sendMessage(chatId, deleteUserText);
-     }).catch(function (err) {
-          sendErrorMessageToBot(chatId);
-     });
+     bot.sendMessage(chatId, message);
 });
 
 bot.on('callback_query', function onCallbackQuery(action) {
@@ -91,10 +84,10 @@ bot.on('callback_query', function onCallbackQuery(action) {
      let data = action.data;
      let selectQuery = `select revenue from users where id = '${userId}';`
 
-     if (data == totalRevenueOptionText) {
-          queryDatabase(selectQuery).then(function (result) {
+     if (data == constants.totalRevenueOptionText) {
+          database.queryDatabase(selectQuery).then(function (result) {
                if (result.rows.length == 0) {
-                    bot.sendMessage(chatId, zeroRevenueText);
+                    bot.sendMessage(chatId, constants.zeroRevenueText);
                } else {
                     let json = JSON.stringify(result.rows[0]);
                     let obj = JSON.parse(json);
@@ -102,35 +95,33 @@ bot.on('callback_query', function onCallbackQuery(action) {
                          amount: obj.revenue
                     };
                     
-                    bot.sendMessage(chatId, `Tus ingresos totales son ${formatter.format(revenue.amount)} €.`);
+                    bot.sendMessage(chatId, `Tus ingresos totales son ${helpers.formatter.format(revenue.amount)} €.`);
                }
           }).catch(function (err) {
                sendErrorMessageToBot(chatId);
           });
-     } else if (data == cancelText) {
-          bot.sendMessage(chatId, cancelActionsText);
+     } else if (data == constants.cancelText) {
+          bot.sendMessage(chatId, constants.cancelActionsText);
      }
 });
 
-function queryDatabase(query) {
-     return new Promise(function (resolve, reject) {
-          pool.connect(function(err, client, done) {
-               if (err) {
-                    reject(err);
-               } else {
-                    client.query(query, function(error, result) {
-                         done();
-                         if (error) {
-                              reject(error);
-                         } else {
-                              resolve(result);
-                         }
-                    });
-               }
-          });
-     });
+/**
+ * Scheduler functions.
+ */
+cron.schedule('* * * * *', () => {
+     log("Action every 1 minute.");
+});
+
+/**
+ * Helper functions.
+ */
+function sendErrorMessageToBot(chatId) {
+     bot.sendMessage(chatId, constants.generalErrorText);
 };
 
-function sendErrorMessageToBot(chatId) {
-     bot.sendMessage(chatId, generalErrorText);
+/**
+ * Logs.
+ */
+function log(message) {
+     // console.log(message);
 };
---
+++ src/constants.js
@@ -0,0 +1,12 @@
+module.exports = {
+    startText : "\nEscribe la barra / para ver en qué te puedo ayudar.\n\nMás información: https://github.com/ArtCC/myfinancesbot-artcc",
+    revenueUpdateText : "Tus ingresos totales han sido actualizados correctamente.",
+    revenueInsertText : "Tus ingresos totales han sido añadidos correctamente.",
+    deleteUserText : "Tu usuario ha sido eliminado correctamente.",
+    generalErrorText : "¡Vaya! Parece que ha habido un problema con tu solicitud. Inténtalo de nuevo por favor.",
+    actionsTitleText : "Elige una de las opciones disponibles",
+    totalRevenueOptionText : "Ingresos",
+    zeroRevenueText : "Tus ingresos totales son 0 €.",
+    cancelText : "Cancelar",
+    cancelActionsText : "De acuerdo."
+};
