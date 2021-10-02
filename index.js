cf592e7ec7ef23e588c9fdf1c291a363cf95b07a
Arturo Carretero Calvo <10163049+ArtCC@users.noreply.github.com>
2021-10-02 14:55:38 +0200

    Add donate option.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const TelegramBot = require("node-telegram-bot-api");
 const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, {
      polling: true
 });
-const constants = require('./constants');
+const constants = require('./src/constants');
 const cron = require('node-cron');
 const database = require('./src/database');
 const helpers = require('./src/helpers');
